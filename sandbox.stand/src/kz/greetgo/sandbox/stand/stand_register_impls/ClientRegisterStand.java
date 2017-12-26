aa20b406bc988d7c128b54e2212866e77dd67f64
ndemina <ndemina@greet-go.com>
2017-12-26 16:04:35 +0600

    Заполнила таблицу данными

--- sandbox.stand/src/kz/greetgo/sandbox/stand/stand_register_impls/ClientRegisterStand.java
+++ sandbox.stand/src/kz/greetgo/sandbox/stand/stand_register_impls/ClientRegisterStand.java
@@ -12,17 +12,22 @@ public class ClientRegisterStand implements ClientRegister {
   @Override
   public List<ClientRecord> list() {
     List<ClientRecord> ret = new ArrayList<>();
-    ret.add(record("a10", "Иванов Иван"));
-    ret.add(record("a11", "Петров Иван"));
-    ret.add(record("a12", "Сидоров Иван"));
-    ret.add(record("a13", "Абрамов Иван"));
+    ret.add(record("a10", "Иванов Иван", "Сангвиник", 27, 100000, 150000, 50000));
+    ret.add(record("a11", "Петров Иван", "Меланхолик", 45, 200000, 250000, 150000));
+    ret.add(record("a12", "Сидоров Иван", "Холерик", 20, 75000, 100000,25000 ));
+    ret.add(record("a13", "Абрамов Иван", "Флегматик", 30, 120000, 200000, 2000));
     return ret;
   }
 
-  private ClientRecord record(String id, String fio) {
+  private ClientRecord record(String id, String fio, String charm, int age, int totalBalance, int maxBalance, int minBalance) {
     ClientRecord ret = new ClientRecord();
     ret.id = id;
     ret.fio = fio;
+    ret.charm = charm;
+    ret.age = age;
+    ret.totalBalance = totalBalance;
+    ret.maxBalance = maxBalance;
+    ret.minBalance = minBalance;
     return ret;
   }
 }
